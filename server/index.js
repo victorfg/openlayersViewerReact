2f8af67b099541fd9ecc21b6d0c9bc95c06326e0
victorfg <victor.fernandez.gayan@gmail.com>
2021-08-22 20:17:24 +0200

    .

--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const path = require('path');
 
-const db = mysql.createPool({
-  host:'localhost',
-  user:'root',
-  password:'VFIskra1234',
-  database:'vfernandsugeo'
+var getConnection = require('./connection/db');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
 app.get("/api", (req, res) => {
-  const sqlSelect = "SELECT * FROM geograf";
-  db.query(sqlSelect, (err, result)=> {
-    console.log(result);
-    res.send(result);
-  })
-
+  getConnection(function (err, con) {
+    if(err) { console.log(err) }
+    var userQuery = "SELECT * FROM geograf";
+    console.log("con: " + con); //displays undefined
+    con.query(userQuery, (err, result)=> {
+      //console.log(result);
+      res.send(result);
+    });
+  });
 });
 
-app.listen(3001, ()=> {
+//* DEVELOPMENT *//
+
+/*app.listen(3001, ()=> {
+  console.log('runing port 3001')
+})*/
+
+//* DEPLOYMENT *//
+
+app.listen(process.env.PORT || PORT, ()=> {
   console.log('runing port 3001')
 })
